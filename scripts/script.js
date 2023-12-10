8815b647bf2cc5b4d7e65813f42ff8af56c72c23
ZiggyvdWeetering <150699127+ZiggyvdWeetering@users.noreply.github.com>
2023-12-10 14:54:07 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,13 @@
 console.log("hi");
 
 var h1element = document.querySelector("body main section:nth-of-type(1) h2");
-
 var carouselnav1 = document.querySelector("section:nth-of-type(1) nav ul li:nth-of-type(1) a")
 var carouselnav2 = document.querySelector("section:nth-of-type(1) nav ul li:nth-of-type(2) a")
 var carouselnav3 = document.querySelector("section:nth-of-type(1) nav ul li:nth-of-type(3) a")
 var carouselnav4 = document.querySelector("section:nth-of-type(1) nav ul li:nth-of-type(4) a")
+
+var lightModeCheck = document.querySelector("header input");
+
 
 
 if (carouselnav1) {
@@ -22,37 +24,33 @@ if (carouselnav4) {
   carouselnav4.addEventListener("click", vierde_h2);
 }
 
-
+// dom monipulatie fucntie voor carousel
 function eerste_h2(){
-    console.log("test");
-    h1element.textContent = "Trolls band together";
-    }
+  console.log("test");
+  h1element.textContent = "Trolls band together";
+}
 
 function tweede_h2(){
-    console.log("test");
-    h1element.textContent = "the bad guys: a very bad holiday";
-    }
+  console.log("test");
+  h1element.textContent = "the bad guys: a very bad holiday";
+}
 
 function derde_h2(){
-    console.log("test");
-    h1element.textContent = "go, dog. go!";
-    }
+  console.log("test");
+  h1element.textContent = "go, dog. go!";
+}
 
 function vierde_h2(){
-    console.log("test");
-    h1element.textContent = "gabby's dollhouse";
-    }
+  console.log("test");
+  h1element.textContent = "gabby's dollhouse";
+}
 
 
 
 
-    
 
-var lightModeCheck = document.querySelector("header input");
-
-
+// ligh dark functie
 lightModeCheck.addEventListener('change', veranderd);
-
 
 function veranderd() {
   if( lightModeCheck.checked ) {
@@ -76,4 +74,20 @@ if( localStorage.getItem("colorMode") ) {
 
 
 
+// https://www.youtube.com/watch?v=T33NN_pPeNI
+// scroll animaties, ik heb geen idee hoe dit gedeelte precies werkt. ik weet alleen dat er gecgeckt word wanneer de class op het beeldscherm is
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); 
+    } else {
+      entry.target.classList.remove('show'); 
+    }
+  });
+}, { threshold: 0.5 }); // Adjust the threshold as needed
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
 
